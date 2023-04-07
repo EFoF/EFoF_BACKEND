6617f45bd786f2f53baa3332e5ee5a41365e772f
Owen-Choi <oldstyle482@gmail.com>
2023-04-07 15:54:53 +0900

    [ADD] 프로젝트 구조 재배치 - 3

--- src/main/java/com/service/surveyservice/domain/answer/model/Answer.java
+++ src/main/java/com/service/surveyservice/domain/answer/model/Answer.java
@@ -7,11 +7,13 @@ import lombok.ToString;
 
 import javax.persistence.*;
 
-@NoArgsConstructor // 파라미터가 없는 생성자를 생성
-@AllArgsConstructor // 클래스에 존재하는 모든 필드에 대한 생성자를 자동으로 생성
-@ToString
+
 @Entity
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Answer {
+
     @Id // Primary Key 지정
     @Column(name = "Answer_ID") // 컬럼 지정
     @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT 설정

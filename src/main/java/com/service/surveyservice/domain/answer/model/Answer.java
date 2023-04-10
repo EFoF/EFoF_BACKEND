9dc261a2e1772cb174ca8a13d78a5b4fb19992cc
Owen-Choi <oldstyle482@gmail.com>
2023-04-10 18:08:37 +0900

    [ADD] 동혁 피드백 수정

--- src/main/java/com/service/surveyservice/domain/answer/model/Answer.java
+++ src/main/java/com/service/surveyservice/domain/answer/model/Answer.java
@@ -3,6 +3,7 @@ package com.service.surveyservice.domain.answer.model;
 import com.service.surveyservice.domain.member.model.MemberSurvey;
 import com.service.surveyservice.domain.model.BaseTimeEntity;
 import com.service.surveyservice.domain.question.model.Question;
+import com.service.surveyservice.domain.question.model.QuestionOption;
 import lombok.*;
 
 import javax.persistence.*;
@@ -20,16 +21,20 @@ public class Answer extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT 설정
     private Long id;
 
-    @OneToOne
-    @JoinColumn
+    @ManyToOne
+    @JoinColumn(name = "question_id")
     private Question question;
 
     @ManyToOne
     @JoinColumn(name = "memberSurvey_id")
     private MemberSurvey memberSurvey;
 
-    private int multipleChoiceAnswer;
+    // 객관식 답변
+    @ManyToOne
+    @JoinColumn(name = "questionChoice_id")
+    private QuestionOption questionOption;
 
-    private String essayQuestionAnswer;
+    // 주관식 답변
+    private String answerSentence;
 }
 

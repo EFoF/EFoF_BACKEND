3623398bd390d3d35c5c887ef802682c745e36ee
Owen-Choi <oldstyle482@gmail.com>
2023-04-10 23:04:57 +0900

    [ADD] cors 설정, passwordEncoder 설정, 인증 서비스 추가

--- src/main/java/com/service/surveyservice/domain/member/application/AuthService.java
+++ src/main/java/com/service/surveyservice/domain/member/application/AuthService.java
@@ -1,6 +1,9 @@
 package com.service.surveyservice.domain.member.application;
 
 import com.service.surveyservice.domain.member.dao.MemberRepository;
+import com.service.surveyservice.domain.member.dto.MemberDTO;
+import com.service.surveyservice.domain.member.model.Member;
+import com.service.surveyservice.global.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -9,6 +12,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import static com.service.surveyservice.domain.member.dto.MemberDTO.*;
+import static com.service.surveyservice.global.common.ResponseConstants.CREATED;
 
 @Service
 @Slf4j
@@ -16,12 +21,21 @@ import javax.transaction.Transactional;
 public class AuthService {
 
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
-//    private final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
     private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
     private final RedisTemplate<String, String> redisTemplate;
 
+    @Transactional
+    public String signUp(SignUpRequest signUpRequest) {
+        signUpRequest.encrypt(passwordEncoder);
+        Member member = signUpRequest.toEntity();
+        memberRepository.save(member);
+        return CREATED;
+    }
+
 //    @Transactional
-//    public String signUp()
+//    public UserLoginDto login
 
 
 }

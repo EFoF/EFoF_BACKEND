799373d43756fdaf30f4c10ba1f28f34b8e631c2
Owen-Choi <oldstyle482@gmail.com>
2023-04-16 23:14:14 +0900

    [ADD] 이메일 인증 전송자 변경

--- src/main/java/com/service/surveyservice/domain/member/application/EmailCertificationService.java
+++ src/main/java/com/service/surveyservice/domain/member/application/EmailCertificationService.java
@@ -50,7 +50,7 @@ public class EmailCertificationService {
         msg += "</div>";
 
         message.setText(msg, "utf-8", "html");
-        message.setFrom(new InternetAddress("oldstyle4@naver.com", "COKIRI_admin"));
+        message.setFrom(new InternetAddress("oldstyle4@naver.com", "DOKSEOL_admin"));
         return message;
     }
 

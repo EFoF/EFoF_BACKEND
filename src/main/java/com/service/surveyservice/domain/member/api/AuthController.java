f653aefedc3ebc924c8fd5d38577f9987a33669f
Owen-Choi <oldstyle482@gmail.com>
2023-04-16 22:03:07 +0900

    [ADD] Message Body를 포함한 요청에 대해 GET -> POST로 변경

--- src/main/java/com/service/surveyservice/domain/member/api/AuthController.java
+++ src/main/java/com/service/surveyservice/domain/member/api/AuthController.java
@@ -11,11 +11,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.service.surveyservice.domain.member.dto.MailDTO.*;
 import static com.service.surveyservice.domain.member.dto.MemberDTO.*;
 import static com.service.surveyservice.domain.token.dto.TokenDTO.*;
 import static com.service.surveyservice.global.common.constants.AuthenticationConstants.LOGOUT;
@@ -39,52 +39,52 @@ public class AuthController {
     }
 
     // 이메일 중복 확인
-    @GetMapping(value = "/auth/check-email")
+    @PostMapping(value = "/auth/check-email")
     public ResponseEntity<RedunCheckDTO> emailDuplicationCheck(@RequestBody CheckEmailRequestDTO checkEmailRequestDTO) {
         return new ResponseEntity<>(memberService.existsByEmail(checkEmailRequestDTO.getEmail()), HttpStatus.OK);
     }
 
     // 닉네임 중복 확인
-    @GetMapping(value = "/auth/check-nickname")
+    @PostMapping(value = "/auth/check-nickname")
     public ResponseEntity<RedunCheckDTO> nicknameDuplicationCheck(@RequestBody CheckNicknameRequestDTO checkNicknameRequestDTO) {
         return new ResponseEntity<>(memberService.existsByNickname(checkNicknameRequestDTO.getNickname()), HttpStatus.OK);
     }
 
     // 이메일 인증
-    @GetMapping(value = "/auth/mailConfirm")
-    public ResponseEntity<MailDTO.EmailSentDTO> mailConfirm(@RequestBody MailDTO.ConfirmEmailDTO confirmEmailDTO) throws Exception {
+    @PostMapping(value = "/auth/mailConfirm")
+    public ResponseEntity<EmailSentDTO> mailConfirm(@RequestBody ConfirmEmailDTO confirmEmailDTO) throws Exception {
         emailCertificationService.sendSimpleMessage(confirmEmailDTO.getEmail());
-        return new ResponseEntity<>(MailDTO.EmailSentDTO.builder().email(confirmEmailDTO.getEmail()).success(true).build(), HttpStatus.OK);
+        return new ResponseEntity<>(EmailSentDTO.builder().email(confirmEmailDTO.getEmail()).success(true).build(), HttpStatus.OK);
     }
 
     // 코드 인증 요청 : 이메일 인증에서 인증 코드 생성과 동시에 이메일을 발송하고, 이 api로 코드가 일치한지 확인한다.
-    @GetMapping(value = "/auth/codeConfirm")
-    public ResponseEntity<MailDTO.CodeConfirmDTO> codeConfirm(@RequestBody MailDTO.EmailConfirmCodeDTO emailConfirmCodeDTO) {
+    @PostMapping(value = "/auth/codeConfirm")
+    public ResponseEntity<CodeConfirmDTO> codeConfirm(@RequestBody EmailConfirmCodeDTO emailConfirmCodeDTO) {
         return new ResponseEntity<>(emailCertificationService.confirmCode(emailConfirmCodeDTO), HttpStatus.OK);
     }
 
     // 이메일 찾기
-    @GetMapping(value = "/auth/find/email")
-    public ResponseEntity<MailDTO.EncryptEmailDTO> findEmail(@RequestBody MailDTO.FindEmailDTO findEmailDTO) {
-        MailDTO.EncryptEmailDTO userEmail = memberService.findUserEmail(findEmailDTO);
+    @PostMapping(value = "/auth/find/email")
+    public ResponseEntity<EncryptEmailDTO> findEmail(@RequestBody FindEmailDTO findEmailDTO) {
+        EncryptEmailDTO userEmail = memberService.findUserEmail(findEmailDTO);
         return new ResponseEntity<>(userEmail, HttpStatus.OK);
     }
 
     // 비밀번호 찾기
-    @GetMapping(value = "/auth/find/password")
+    @PostMapping(value = "/auth/find/password")
     public ResponseEntity<ReturnPasswordDTO> findPassword(@RequestBody FindPasswordRequestDTO findPasswordRequestDTO) {
         ReturnPasswordDTO returnPasswordDTO = memberService.findUserPassword(findPasswordRequestDTO);
         return new ResponseEntity<>(returnPasswordDTO, HttpStatus.OK);
     }
 
     // 로그아웃 리디렉션
-    @GetMapping(value = "/logout-redirect")
+    @PostMapping(value = "/logout-redirect")
     public ResponseEntity<String> loginRedirect() {
         return new ResponseEntity<>(LOGOUT, HttpStatus.OK);
     }
 
     // 토큰 재발급
-    @GetMapping(value = "/auth/reissue")
+    @PostMapping(value = "/auth/reissue")
     public ResponseEntity<TokenIssueDTO> reissue(@RequestBody AccessTokenDTO accessTokenDTO) {
         return new ResponseEntity<>(authService.reissue(accessTokenDTO), HttpStatus.OK);
     }

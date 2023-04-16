f4ca80d3e723e2b354c9ad4b5cbdd7a51d5ac901
Owen-Choi <oldstyle482@gmail.com>
2023-04-16 16:19:16 +0900

    [ADD] Branch 전환

--- src/main/java/com/service/surveyservice/domain/member/api/MemberController.java
+++ src/main/java/com/service/surveyservice/domain/member/api/MemberController.java
@@ -4,13 +4,19 @@ import com.service.surveyservice.domain.member.application.MemberService;
 import com.service.surveyservice.domain.member.dao.MemberCustomRepositoryImpl;
 import com.service.surveyservice.domain.member.dao.MemberRepository;
 import com.service.surveyservice.domain.member.dto.MemberDTO;
+import com.service.surveyservice.domain.survey.application.SurveyService;
+import com.service.surveyservice.domain.survey.dao.SurveyRepository;
+import com.service.surveyservice.domain.survey.dto.SurveyDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static com.service.surveyservice.domain.member.dto.MemberDTO.*;
+import static com.service.surveyservice.domain.survey.dto.SurveyDTO.*;
 
 @Slf4j
 @RestController
@@ -18,6 +24,9 @@ import static com.service.surveyservice.domain.member.dto.MemberDTO.*;
 public class MemberController {
 
    private final MemberService memberService;
+   private final SurveyService surveyService;
+   private final SurveyRepository surveyRepository;
+
    private final MemberRepository memberRepository;
    private final MemberCustomRepositoryImpl memberCustomRepository;
 
@@ -48,5 +57,10 @@ public class MemberController {
        return new ResponseEntity<>(memberService.getMemberDetail(memberId), HttpStatus.OK);
    }
 
+   // 특정 사용자가 생성한 설문조사 조회 - 페이지네이션 적용
+//    @GetMapping(value = "/member/form/author/{memberId}")
+//    public ResponseEntity<Page<SurveyInfoDTO>> getAuthorSurveyInfo(@PathVariable(name = "memberId") Long memberId, Pageable pageable) {
+//
+//    }
 
 }

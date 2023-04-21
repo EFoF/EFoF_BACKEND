cf5a27338c5dbd8b181632ea3e66528b8274220d
Owen-Choi <oldstyle482@gmail.com>
2023-04-22 03:29:17 +0900

    [ADD] 사용자가 참여한 설문 조회 서비스 로직 구현 - 1

--- src/main/java/com/service/surveyservice/domain/survey/api/SurveyController.java
+++ src/main/java/com/service/surveyservice/domain/survey/api/SurveyController.java
@@ -1,13 +1,16 @@
 package com.service.surveyservice.domain.survey.api;
 
 import com.service.surveyservice.domain.member.application.MemberService;
+import com.service.surveyservice.domain.survey.application.MemberSurveyService;
 import com.service.surveyservice.domain.survey.application.SurveyService;
 import com.service.surveyservice.domain.survey.dto.SurveyDTO;
+import com.service.surveyservice.domain.survey.model.MemberSurvey;
 import com.service.surveyservice.global.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +24,7 @@ public class SurveyController {
 
     private final MemberService memberService;
     private final SurveyService surveyService;
+    private final MemberSurveyService memberSurveyService;
 
     @PostMapping(value = "/form")
     public ResponseEntity<SurveyInfoDTO> createSurvey(@RequestBody CreateSurveyRequestDTO createSurveyRequestDTO) {
@@ -28,4 +32,5 @@ public class SurveyController {
         SurveyInfoDTO surveyInfoDTO = surveyService.createSurvey(createSurveyRequestDTO, currentMemberId);
         return new ResponseEntity<>(surveyInfoDTO, HttpStatus.CREATED);
     }
+
 }

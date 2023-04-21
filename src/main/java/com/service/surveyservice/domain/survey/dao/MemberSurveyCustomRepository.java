e191d059a851064ed40b467430d514119efdcd36
Owen-Choi <oldstyle482@gmail.com>
2023-04-22 02:48:16 +0900

    [ADD] 사용자가 참여한 설문 조회 페이지네이션

---
+++ src/main/java/com/service/surveyservice/domain/survey/dao/MemberSurveyCustomRepository.java
@@ -0,0 +1,12 @@
+package com.service.surveyservice.domain.survey.dao;
+
+import com.service.surveyservice.domain.survey.dto.MemberSurveyDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import static com.service.surveyservice.domain.survey.dto.MemberSurveyDTO.*;
+
+public interface MemberSurveyCustomRepository {
+
+    Page<MemberSurveyInfoDTO> findByMemberIdWithPage(Long id, Pageable pageable);
+}

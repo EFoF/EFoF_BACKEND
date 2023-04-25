8e973604cb1bec3f4386cd0c4a5ac38f01bde535
Owen-Choi <oldstyle482@gmail.com>
2023-04-25 14:28:23 +0900

    [ADD] jwtFilter 수정 - 권한 정상화

--- src/main/java/com/service/surveyservice/global/filter/JwtFilter.java
+++ src/main/java/com/service/surveyservice/global/filter/JwtFilter.java
@@ -1,6 +1,7 @@
 package com.service.surveyservice.global.filter;
 
 import com.service.surveyservice.global.jwt.JwtTokenProvider;
+import com.service.surveyservice.global.util.CookieUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -11,12 +12,12 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static com.service.surveyservice.global.common.constants.JwtConstants.AUTHORIZATION_HEADER;
-import static com.service.surveyservice.global.common.constants.JwtConstants.BEARER_PREFIX;
+import static com.service.surveyservice.global.common.constants.JwtConstants.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -30,24 +31,29 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         String jwt = resolveToken(request);
         log.info("APi 호출됨");
-        if(jwt != null) {
-            Boolean hasKey = redisTemplate.hasKey(jwt);
-            if(hasKey == null) {
-                hasKey = false;
-            }
-            if(StringUtils.hasText(jwt) && hasKey && jwtTokenProvider.validateToken(jwt)) {
-                Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            }
+//        if(jwt != null) {
+//            if(StringUtils.hasText(jwt) && hasKey && jwtTokenProvider.validateToken(jwt)) {
+//                Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
+//                SecurityContextHolder.getContext().setAuthentication(authentication);
+//            }
+//        }
+        if (StringUtils.hasText(jwt) && jwtTokenProvider.validateToken(jwt)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
         filterChain.doFilter(request, response);
     }
 
     private String resolveToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
-        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
-            return bearerToken.substring(7);
+//        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+//        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+//            return bearerToken.substring(7);
+//        }
+//        return null;
+        Cookie bearerToken = CookieUtil.getCookie(request, ACCESS_TOKEN).orElse(null);
+        if(bearerToken != null) {
+            return bearerToken.getValue();
         }
         return null;
     }

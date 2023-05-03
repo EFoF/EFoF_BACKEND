f88994aa00608b77b49ae47148ae3299ab90dd55
임동혁 <idh1007@naver.com>
2023-05-03 17:19:03 +0900

    ES-83 [REFACTOR] 예외 처리 수정

--- src/main/java/com/service/surveyservice/domain/question/exception/response/QuestionErrorResponse.java
+++ src/main/java/com/service/surveyservice/domain/question/exception/response/QuestionErrorResponse.java
@@ -1,16 +1,22 @@
 package com.service.surveyservice.domain.question.exception.response;
 
+import com.service.surveyservice.domain.question.exception.exceptions.QuestionNotFoundException;
+import com.service.surveyservice.domain.question.exception.exceptions.QuestionOptionNotFoundException;
 import com.service.surveyservice.global.error.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public class QuestionErrorResponse {
 
+
     public static final ResponseEntity<ErrorResponse> QUESTION_NOT_FOUND = new ResponseEntity<>(ErrorResponse.builder()
-            .status(HttpStatus.NOT_FOUND)
+            .exceptionName(QuestionNotFoundException.class.getSimpleName())
             .message("해당 질문은 존재하지 않는 질문입니다.")
             .build(),HttpStatus.NOT_FOUND);
 
-
+    public static final ResponseEntity<ErrorResponse> QUESTION_OPTION_NOT_FOUND = new ResponseEntity<>(ErrorResponse.builder()
+            .exceptionName(QuestionOptionNotFoundException.class.getSimpleName())
+            .message("해당 질문 항목은 존재하지 않는 않습니다.")
+            .build(),HttpStatus.NOT_FOUND);
 
 }

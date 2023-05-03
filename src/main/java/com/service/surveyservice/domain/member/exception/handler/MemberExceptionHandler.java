cb13910e33e8170344cbfc979ed80170ad00488d
Owen-Choi <oldstyle482@gmail.com>
2023-05-03 16:17:19 +0900

    [REFACTOR] 예외처리 재수정

--- src/main/java/com/service/surveyservice/domain/member/exception/handler/MemberExceptionHandler.java
+++ src/main/java/com/service/surveyservice/domain/member/exception/handler/MemberExceptionHandler.java
@@ -15,67 +15,67 @@ import static com.service.surveyservice.domain.member.exception.response.MemberE
 public class MemberExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
-    protected final ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
+    protected final ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
         log.error(request.getDescription(false));
         return USER_NOT_FOUND;
     }
 
     @ExceptionHandler(UserNotFoundByUsernameAndNickname.class)
-    protected final ResponseEntity<String> handleUserNotFoundByUsernameAndNicknameException(UserNotFoundByUsernameAndNickname ex, WebRequest request) {
+    protected final ResponseEntity<ErrorResponse> handleUserNotFoundByUsernameAndNicknameException(UserNotFoundByUsernameAndNickname ex, WebRequest request) {
         log.error(request.getDescription(false));
         return USER_NOT_FOUND_BY_USERNAME_AND_NICKNAME;
     }
 
     @ExceptionHandler(UpdateDuplicatedPasswordException.class)
-    protected final ResponseEntity<String> handleUpdateDuplicatedPasswordException(UpdateDuplicatedPasswordException ex, WebRequest request) {
+    protected final ResponseEntity<ErrorResponse> handleUpdateDuplicatedPasswordException(UpdateDuplicatedPasswordException ex, WebRequest request) {
         log.error(request.getDescription(false));
         return UPDATE_DUPLICATED_PASSWORD;
     }
 
     @ExceptionHandler(UpdateDuplicatedNicknameException.class)
-    protected final ResponseEntity<String> handleUpdateDuplicatedNicknameException(UpdateDuplicatedNicknameException ex, WebRequest request) {
+    protected final ResponseEntity<ErrorResponse> handleUpdateDuplicatedNicknameException(UpdateDuplicatedNicknameException ex, WebRequest request) {
         log.error(request.getDescription(false));
         return UPDATE_DUPLICATED_NICKNAME;
     }
 
     @ExceptionHandler(NotMatchingUpdatingNicknameException.class)
-    protected final ResponseEntity<String> handleNotMatchingUpdatingNicknameException(NotMatchingUpdatingNicknameException ex, WebRequest request) {
+    protected final ResponseEntity<ErrorResponse> handleNotMatchingUpdatingNicknameException(NotMatchingUpdatingNicknameException ex, WebRequest request) {
         log.error(request.getDescription(false));
         return NOT_MATCHING_UPDATING_NICKNAME;
     }
 
     @ExceptionHandler(NotMatchingPasswordException.class)
-    protected final ResponseEntity<String> handleNotMatchingPasswordException(NotMatchingPasswordException ex, WebRequest request) {
+    protected final ResponseEntity<ErrorResponse> handleNotMatchingPasswordException(NotMatchingPasswordException ex, WebRequest request) {
         log.error(request.getDescription(false));
         return NOT_MATCHING_PASSWORD;
     }
 
     @ExceptionHandler(NotMatchingCurrentMemberAndRequesterException.class)
-    protected final ResponseEntity<String> handleNotMatchingCurrentMemberAndRequesterException(NotMatchingCurrentMemberAndRequesterException ex, WebRequest request) {
+    protected final ResponseEntity<ErrorResponse> handleNotMatchingCurrentMemberAndRequesterException(NotMatchingCurrentMemberAndRequesterException ex, WebRequest request) {
         log.error(request.getDescription(false));
         return NOT_MATCHING_CURRENT_MEMBER_AND_REQUESTER;
     }
 
     @ExceptionHandler(DuplicatedSignUpException.class)
-    protected final ResponseEntity<String> handleDuplicatedSignUpException(DuplicatedSignUpException ex, WebRequest request) {
+    protected final ResponseEntity<ErrorResponse> handleDuplicatedSignUpException(DuplicatedSignUpException ex, WebRequest request) {
         log.error(request.getDescription(false));
         return DUPLICATED_SIGN_UP;
     }
 
     @ExceptionHandler(InvalidEmailAndPasswordRequestException.class)
-    protected final ResponseEntity<String> handleInvalidEmailAndPasswordRequestException(InvalidEmailAndPasswordRequestException ex, WebRequest request) {
+    protected final ResponseEntity<ErrorResponse> handleInvalidEmailAndPasswordRequestException(InvalidEmailAndPasswordRequestException ex, WebRequest request) {
         log.error(request.getDescription(false));
         return INVALID_EMAIL_AND_PASSWORD_REQUEST;
     }
 
     @ExceptionHandler(NotSignInException.class)
-    protected final ResponseEntity<String> handleNotSignInException(NotSignInException ex, WebRequest request) {
+    protected final ResponseEntity<ErrorResponse> handleNotSignInException(NotSignInException ex, WebRequest request) {
         log.error(request.getDescription(false));
         return NOT_SIGN_IN;
     }
 
     @ExceptionHandler(DuplicatedEmailException.class)
-    protected final ResponseEntity<String> handleDuplicatedEmailException(DuplicatedEmailException ex, WebRequest request) {
+    protected final ResponseEntity<ErrorResponse> handleDuplicatedEmailException(DuplicatedEmailException ex, WebRequest request) {
         log.error(request.getDescription(false));
         return DUPLICATED_EMAIL;
     }

f8c18cb4f56781166926eb4b9ea4d87b646d3290
임동혁 <idh1007@naver.com>
2023-05-03 17:21:36 +0900

    ES-83 [REFACTOR] legacy 제거

--- src/main/java/com/service/surveyservice/domain/survey/application/SurveyService.java
+++ src/main/java/com/service/surveyservice/domain/survey/application/SurveyService.java
@@ -1,33 +1,19 @@
 package com.service.surveyservice.domain.survey.application;
 
 import com.service.surveyservice.domain.member.dao.MemberRepository;
-import com.service.surveyservice.domain.member.exception.exceptions.member.NotMatchingCurrentMemberAndRequesterException;
 import com.service.surveyservice.domain.member.model.Member;
 import com.service.surveyservice.domain.survey.dao.SurveyCustomRepositoryImpl;
 import com.service.surveyservice.domain.survey.dao.SurveyRepository;
-import com.service.surveyservice.domain.survey.dto.MemberSurveyDTO;
-import com.service.surveyservice.domain.survey.dto.SurveyDTO;
-import com.service.surveyservice.domain.survey.exception.ExpireBeforeOpenException;
-import com.service.surveyservice.domain.survey.exception.SurveyConvertException;
 import com.service.surveyservice.domain.survey.model.Survey;
 import com.service.surveyservice.domain.survey.model.SurveyStatus;
 import com.service.surveyservice.global.config.S3Config;
 import com.service.surveyservice.global.error.exception.NotFoundByIdException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.service.surveyservice.domain.survey.dto.MemberSurveyDTO.*;
 import static com.service.surveyservice.domain.survey.dto.SurveyDTO.*;
 import static com.service.surveyservice.global.common.constants.S3Constants.DIRECTORY;
 
@@ -56,29 +42,29 @@ public class SurveyService {
 
     }
 
-    public Page<SurveyInfoDTO> getAuthorSurveyList(Long memberId, Long currentMemberId, Pageable pageable) {
-        // 아이디 검증
-        if(!memberId.equals(currentMemberId)) {
-            throw new NotMatchingCurrentMemberAndRequesterException();
-        }
-        Page<SurveyInfoDTO> surveyInfoDTOPage = surveyCustomRepository.findSurveyInfoDTOByAuthorId(memberId, pageable);
-        return surveyInfoDTOPage;
-    }
-
-    // MemberSurveyService에서 받아온 MemberSurveyInfoDTO의 page를 api에서 사용하기 위해 SurveyInfoDTO로 변환 시켜주는 작업
-    public Page<SurveyInfoDTO> getParticipatedSurveyInfo(Page<MemberSurveyInfoDTO> infoPage) {
-        Page<SurveyInfoDTO> surveyInfoDTOPage = infoPage.map(element -> {
-            Long id = element.getId();
-            return _findSurveyInfoById(id);
-        });
-        return surveyInfoDTOPage;
-    }
-
-    // 내부적으로 사용되는 메서드. id로 SurveyInfoDTO를 찾아온다.
-    private SurveyInfoDTO _findSurveyInfoById (Long surveyId) {
-        SurveyInfoDTO surveyInfoDTO = surveyRepository.findSurveyInfoDTOById(surveyId).orElseThrow(SurveyConvertException::new);
-        return surveyInfoDTO;
-    }
+//    public Page<SurveyInfoDTO> getAuthorSurveyList(Long memberId, Long currentMemberId, Pageable pageable) {
+//        // 아이디 검증
+//        if(!memberId.equals(currentMemberId)) {
+//            throw new NotMatchingCurrentMemberAndRequesterException();
+//        }
+//        Page<SurveyInfoDTO> surveyInfoDTOPage = surveyCustomRepository.findSurveyInfoDTOByAuthorId(memberId, pageable);
+//        return surveyInfoDTOPage;
+//    }
+//
+//    // MemberSurveyService에서 받아온 MemberSurveyInfoDTO의 page를 api에서 사용하기 위해 SurveyInfoDTO로 변환 시켜주는 작업
+//    public Page<SurveyInfoDTO> getParticipatedSurveyInfo(Page<MemberSurveyInfoDTO> infoPage) {
+//        Page<SurveyInfoDTO> surveyInfoDTOPage = infoPage.map(element -> {
+//            Long id = element.getId();
+//            return _findSurveyInfoById(id);
+//        });
+//        return surveyInfoDTOPage;
+//    }
+//
+//    // 내부적으로 사용되는 메서드. id로 SurveyInfoDTO를 찾아온다.
+//    private SurveyInfoDTO _findSurveyInfoById (Long surveyId) {
+//        SurveyInfoDTO surveyInfoDTO = surveyRepository.findSurveyInfoDTOById(surveyId).orElseThrow(SurveyConvertException::new);
+//        return surveyInfoDTO;
+//    }
 
     /**
      * 이미지 업로드
@@ -96,4 +82,6 @@ public class SurveyService {
     public void deleteSurveyImage(String imageUrl) {
         s3Uploader.delete(imageUrl,DIRECTORY);
     }
+
+
 }

f4ffd57b6d16c5bfe99477a2334a1104daa5c450
임동혁 <idh1007@naver.com>
2023-05-03 18:48:54 +0900

    ES-83 [REFACTOR] query 위치 변경

--- src/main/java/com/service/surveyservice/domain/survey/dao/SurveyCustomRepository.java
+++ src/main/java/com/service/surveyservice/domain/survey/dao/SurveyCustomRepository.java
@@ -15,13 +15,5 @@ public interface SurveyCustomRepository {
     Page<SurveyInfoDTO> findSurveyInfoDTOByAuthorId(Long authorId, Pageable pageable);
 
 
-    @Query(value = "SELECT qoi.img_url FROM question_option qo\n" +
-            "INNER JOIN question_option_img qoi ON qo.question_option_img_id = qoi.question_option_img_id\n" +
-            "WHERE qo.question_id IN (\n" +
-            "  SELECT q.question_id FROM question q\n" +
-            "  INNER JOIN section s ON q.section_id = s.section_id\n" +
-            "  WHERE s.survey_id = :survey_id\n" +
-            ") AND qoi.img_url IS NOT NULL;"
-            , nativeQuery = true)
-    List<String> findImgUrlBySurveyId(@Param("survey_id")Long survey_id);
+
 }

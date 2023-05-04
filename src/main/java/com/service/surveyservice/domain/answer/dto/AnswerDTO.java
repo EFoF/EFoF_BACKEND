1490f3b89ccba44d0b26cbca1449eec23495ccaa
jay <wlguswhd0809@naver.com>
2023-05-04 21:44:08 +0900

    ES-37 [ADD] Answer Logic - 진짜 초안(놀리지 않기)

--- src/main/java/com/service/surveyservice/domain/answer/dto/AnswerDTO.java
+++ src/main/java/com/service/surveyservice/domain/answer/dto/AnswerDTO.java
@@ -1,6 +1,23 @@
 package com.service.surveyservice.domain.answer.dto;
 
+import lombok.Builder;
+import lombok.Getter;
 
-public class AnswerDTO {
+@Getter
+@Builder
+public class AnswerDTO { //아직 구현 안 함
+    private Long id;
+    private Long question; // question_id
+    private String memberSurvey; // 특정 사용자가 참여한 특정 설문
+    private String questionOption; // 객관식 답변
+    private String answerSentence; // 주관식 답변
 
+    @Builder
+    public AnswerDTO(Long id, Long question, String memberSurvey, String questionOption, String answerSentence) {
+        this.id = id;
+        this.question = question;
+        this.memberSurvey = memberSurvey;
+        this.questionOption = questionOption;
+        this.answerSentence = answerSentence;
+    }
 }

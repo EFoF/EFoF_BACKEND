2c4b1822c29ab154b7f31fc41265c67891ed39ca
Owen-Choi <oldstyle482@gmail.com>
2023-05-09 23:01:09 +0900

    [REFACTOR] 토큰 오류 수정 최종 완료

--- src/main/java/com/service/surveyservice/global/common/constants/JwtConstants.java
+++ src/main/java/com/service/surveyservice/global/common/constants/JwtConstants.java
@@ -6,8 +6,10 @@ public class JwtConstants {
 
     public static final int ACCESS_TOKEN_COOKIE_EXPIRE_TIME = 60 * 60 * 24 * 7;
 
-    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;
-    public static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60*60*24*7;
+//    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;
+    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 10;
+//    public static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60*60*24*7;
+    public static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 20;
 
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String BEARER_PREFIX = "Bearer ";
